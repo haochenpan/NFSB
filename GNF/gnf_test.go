4dec38d15a480f2366c5d52faeecb44d57d24d39
HaochenPan <phchcc@gmail.com>
2019-06-28 23:21:40 -0400

    add comments

--- GNF/gnf_test.go
+++ GNF/gnf_test.go
@@ -8,43 +8,35 @@ import (
 
 func TestGnfMain(t *testing.T) {
 
-	fPath := "../workload0.txt"
-	phase := "load"
+	fPath := "/Users/roger/go/src/NFSB/Config/workload_template"
+	phase := "loadrun"
 	wl := InitWorkload()
 	if ret := wl.UpdateWorkloadByFile(fPath); ret < 0 {
 		err := fmt.Sprintf("no workload file found on path %q", fPath)
 		t.Fatal(err)
 	}
 
-	var gnfStop bool
+	var bmStat BmStats
 	allToExe := make(chan exeCmd)
 	isDone := make(chan bool)
-	//exeToCtl := make(chan BmStats, 0)
-
-	var bmStat BmStats
 
 	go exeSignThread(allToExe, isDone) // needToWait, exit when isDone is closed
 
 	if phase == "load" {
-		gnfStop, bmStat = benchmarkRoutine(wl, LoadSig, allToExe)
+		_, bmStat = benchmarkRoutine(wl, LoadSig, allToExe)
 	} else if phase == "run" {
-		gnfStop, bmStat = benchmarkRoutine(wl, RunSig, allToExe)
+		_, bmStat = benchmarkRoutine(wl, RunSig, allToExe)
 	} else if phase == "loadrun" {
-		gnfStop, bmStat = benchmarkRoutine(wl, LoadSig, allToExe)
+		_, bmStat = benchmarkRoutine(wl, LoadSig, allToExe)
 		fmt.Println(bmStat.String())
-		gnfStop, bmStat = benchmarkRoutine(wl, RunSig, allToExe)
+		_, bmStat = benchmarkRoutine(wl, RunSig, allToExe)
 	} else {
 		err := fmt.Sprintf("%q is not a valid argument", phase)
-		t.Fatal(err)
+		t.Error(err)
 	}
 
-	//fmt.Println("ret=", ret)
-	//bm := <-exeToCtl
-	fmt.Println("gnfStop=", gnfStop)
 	fmt.Println(bmStat.String())
-	//e := <-allToExe
-	close(isDone)
-	e := <-allToExe
-	fmt.Println("from allToExe", e.arg)
-	fmt.Println("main exits")
+	close(isDone) // signal os signal thread
+	<-allToExe    // wait for os signal thread to exit
+
 }
