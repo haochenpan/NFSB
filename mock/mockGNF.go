14701e458852579bc802d074d0c86d7472fb79f0
Stevie_Wu <wuit@bc.edu>
2019-07-01 12:03:03 -0400

    Add MockUser

---
+++ mock/mockGNF.go
@@ -0,0 +1,16 @@
+package main
+
+import "net"
+
+func main() {
+	var conn net.Conn
+	var err error
+	controllerIP := "10.150.0.6"
+	port := "6668"
+	if conn, err = net.Dial("tcp", controllerIP+":"+port); err != nil {
+		return
+	}
+	for i := 0; i < 5; i++ {
+		conn.Write([]byte("Hey\n"))
+	}
+}
